7cb6b864afc1380b7566aa1af1400c8a8d757779
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2024-12-13 13:14:40 +0530

    datatypes of js

---
+++ WEBD/3.JS/01_basics/02_datatypes.js
@@ -0,0 +1,54 @@
+"use strict"; //use strict mode to avoid errors
+//treat all JS code as new version of JS
+
+
+//(alert("Hello World!"));//we are using nodejs ,not browser
+
+
+console.log("Hello World!");//Hello World!
+
+
+//original documentation at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+//tc39.es/ecma262/#sec-ecmascript-language-types
+
+
+//Primitive Data Types
+//1. Number => 2 to the power of 53-1
+//2. String => "Hello World!"
+//3. Boolean => true or false
+//4. Undefined => undefined
+//5. Null => null  //typeof null => object
+
+// null vs undefined => null is assigned by the programmer, undefined is assigned by the JS engine
+// null is an object, undefined is a type   
+
+
+//6. Symbol => Symbol('foo') //unique value
+//7. BigInt
+//8. BigInt64Array
+//9. BigUint64Array
+
+
+
+
+//10. Object => {key:value} //collection of key value pairs
+
+
+//11. Function
+//12. Array
+//13. Date
+//14. RegExp
+//15. Error
+//16. Map
+
+
+//typeof operator
+//returns the type of the variable
+
+let a=10;
+console.log(typeof a);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+
