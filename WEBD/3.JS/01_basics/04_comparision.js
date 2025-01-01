a271c0eaaa81575d0dfd88ffdfac2e39d0180bb4
Amulya Kumar Maurya <akmewtriple7@gmail.com>
2025-01-01 22:57:17 +0530

    //edit in shadows

---
+++ WEBD/3.JS/01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(1==1);//true
+// console.log(1==true);//true
+// console.log(0==false);//true
+
+// console.log(2>1);//true
+// console.log(2<1);//false
+// console.log(2>=1);//true
+// console.log(2<=1);//false
+// console.log(2!=1);//true
+
+// console.log(2==="2");//false
+// console.log("2">1);//true
+// console.log("2"<1);//false
+
+// console.log(2=="2");//true
+// console.log("02"==2);//true
+
+console.log(null>0);//false
+console.log(null<0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
---
+++ WEBD/3.JS/01_basics/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+const accountId= 123456;
+let accountEmail="Amulya@gmail.com";
+var accountpassword="123456";
+acountcity="Bangalore";
+
+
+accountId= 1234567;
+
+console.log(accountId);
+
+console.log(accountEmail);
+
+
+
+
